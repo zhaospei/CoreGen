287742eca2d74485a5dceb7a8518c1fb28ebfb06
Dung T.Bui <dungbuit1k28@gmail.com>
2023-09-01 19:34:07 +0700

    Update meteor.py

--- evaluation/meteor/meteor.py
+++ evaluation/meteor/meteor.py
@@ -15,7 +15,7 @@ METEOR_JAR = 'meteor-1.5.jar'
 class Meteor:
 
     def __init__(self):
-        self.meteor_cmd = ['java', '-jar', '-Xmx2G', METEOR_JAR, \
+        self.meteor_cmd = ['/usr/bin/java', '-jar', '-Xmx2G', METEOR_JAR, \
                 '-', '-', '-stdio', '-l', 'en', '-norm']
         self.meteor_p = subprocess.Popen(self.meteor_cmd, \
                 cwd=os.path.dirname(os.path.abspath(__file__)), \
