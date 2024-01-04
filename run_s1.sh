fa6320cae62e40b796e36ac6a417c938e1715323
Mikasa Hackerman :) <dungbuit1k28@gmail.com>
2024-01-04 07:14:05 +0700

    Create run_s1.sh

---
+++ run_s1.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 LAYER_NUM HEAD_NUM (Example: ./run_all.sh 2 6)" >&2
+    exit 1
+fi
+
+DATAPATH=./data
+SAVEPATH=./exp
+
+# hyper-parameter configuration
+LAYER=$1
+HEAD=$2
+STAGE1_BATCH_SIZE=2
+STAGE2_BATCH_SIZE=32
+STAGE1_EPOCH=30
+STAGE2_EPOCH=100
+MASK_RATE=0.5
+MAX_LENGTH=512
+MIN_WORD_COUNT=0
+
+# preparing directories
+mkdir -p ${SAVEPATH}
+mkdir -p ${SAVEPATH}/vocab/
+mkdir -p ${SAVEPATH}/log/
+mkdir -p ${SAVEPATH}/pretrain/
+mkdir -p ${SAVEPATH}/finetune/
+
+# preprocessing
+python preprocess.py -train_src ${DATAPATH}/cleaned.train.diff -train_tgt ${DATAPATH}/cleaned.train.msg -valid_src ${DATAPATH}/cleaned.valid.diff -valid_tgt ${DATAPATH}/cleaned.valid.msg -save_data ${SAVEPATH}/vocab/vocab -max_len ${MAX_LENGTH} -min_word_count ${MIN_WORD_COUNT} -share_vocab
+python pretrain.py -train_src ${DATAPATH}/cleaned.train.diff -valid_src ${DATAPATH}/cleaned.valid.diff -vocab ${SAVEPATH}/vocab/vocab -save_data ${SAVEPATH}/vocab/pretrain_vocab -mask_rate ${MASK_RATE} -max_len ${MAX_LENGTH} -min_word_count ${MIN_WORD_COUNT}  
+
+# stage I training
+python train.py -data ${SAVEPATH}/vocab/pretrain_vocab -save_model ${SAVEPATH}/pretrain/pretrain_${LAYER}layer_${HEAD}head_${MASK_RATE}mask -log ${SAVEPATH}/log/pretrain_${LAYER}layer_${HEAD}head_${MASK_RATE}mask -save_mode best -proj_share_weight -embs_share_weight -label_smoothing -epoch ${STAGE1_EPOCH} -batch_size ${STAGE1_BATCH_SIZE} -n_head ${HEAD} -n_layers ${LAYER}
